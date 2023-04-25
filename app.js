c31dcfd56f166c534c2fb6267e86f028b0f73b91
usamah <hakaro375@gmail.com>
2023-04-25 12:10:57 +0700

    level 6 Oauth with google

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -25,6 +27,21 @@ app.use(session({
 // Pasport initilazed and session
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        console.log(profile);
+      return cb(err, user);
+    });
+  }
+));
 
 // MongoDB atlas URI
 const dbUsername = process.env.DB_USERNAME
@@ -47,22 +64,43 @@ mongoose.connect(uri, {
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
+    googleId: String,
   });
+
 // Pasport Local mongoose
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 // Mongoose model based on userSchema
 const User = new mongoose.model("User", userSchema);
 
+//Passport create strategy serialize and deserialize
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+      done(null, user.id);
+  });
+  
+passport.deserializeUser(async function(id, done) {
+    try {
+        const user = await User.findById(id);
+        done(null, user)
+    } catch (err) {
+        done(err, null)
+    }
+  });
 
 
-// Getting home, login and register page
+// Getting home, login, register, secrets, and logout
 app.get("/",function(req,res){
     res.render("home");
 });
+app.get("/auth/google", passport.authenticate("google", {scope: ["profile"]}));
+app.get("/auth/google/secrets", passport.authenticate("google", { failureRedirect: "/login" }),
+    function(req, res) {
+        // Successful authentication, redirect to secrets page
+        res.redirect("/secrets");
+    });
 app.get("/register",function(req,res){
     res.render("register");
 });
